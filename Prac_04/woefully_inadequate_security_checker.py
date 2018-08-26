b99396e3066dcd662f055444d9d1d119aefa6188
Thomas-Wallis <thomaswallis150598@hotmail.com>
2018-08-26 18:53:31 +1000

    "Completed bad security checker"

---
+++ Prac_04/woefully_inadequate_security_checker.py
@@ -0,0 +1,6 @@
+user_names = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+users_name = input("Please enter your name here: ")
+if users_name in user_names:
+    print("Access granted")
+else:
+    print("Access denied")
