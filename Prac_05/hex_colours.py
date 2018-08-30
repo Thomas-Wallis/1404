f1038c4335952f86a328835a9d47548587f2482b
Thomas-Wallis <thomaswallis150598@hotmail.com>
2018-08-30 15:50:47 +1000

    "Hex colours complete"

---
+++ Prac_05/hex_colours.py
@@ -0,0 +1,12 @@
+COLOURS_AND_HEX_CODES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb",
+                         "AntiqueWhite2": "#eedfcc", "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378",
+                         "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6", "aquamarine4": "#458b74",
+                         "azure1": "#f0ffff"}
+
+colour = input("Colour: ")
+while colour != "":
+    if colour in COLOURS_AND_HEX_CODES:
+        print("{} - {}".format(colour, COLOURS_AND_HEX_CODES[colour]))
+    else:
+        print("Don't have that one")
+    colour = input("Colour: ")
