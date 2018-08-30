bfa29c9cac4a2fdb0e826ebe702eed3842bc4368
Thomas-Wallis <thomaswallis150598@hotmail.com>
2018-08-30 11:32:47 +1000

    "Changed the name of one variable"

--- Prac_04/woefully_inadequate_security_checker.py
+++ Prac_04/woefully_inadequate_security_checker.py
@@ -1,6 +1,6 @@
 user_names = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
-users_name = input("Please enter your name here: ")
-if users_name in user_names:
+name = input("Please enter your name here: ")
+if name in user_names:
     print("Access granted")
 else:
     print("Access denied")
