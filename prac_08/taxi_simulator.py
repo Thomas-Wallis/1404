e754f878c1da2fbde20460b339d734ff26d426f0
Thomas-Wallis <thomaswallis150598@hotmail.com>
2018-10-08 00:19:25 +1000

    Taxi simulator completed. Added display taxis function

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -1,22 +1,59 @@
+from prac_08.car import Car
 from prac_08.taxi import Taxi
 from prac_08.silver_service_taxi import SilverServiceTaxi
 
 
+MENU = "(Q)uit, (C)hoose taxi, (D)rive"
 def main():
-    taxis = Taxi("Basic Taxi", 100), SilverServiceTaxi("Siver Service Taxi", 100, 3)
-    menu = input("Select a taxi from the list below to see exact details.\n(B)asic Taxi\n(S)ilver Service Taxi\n(Q)uit\n>>>").upper()
-    while menu != "Q":
-        if input == "B":
-            print("Basic Taxi")
-            menu = input("Select a taxi from the list below to see exact details.\n(B)asic Taxi\n(S)ilver Service Taxi\n(Q)uit\n>>>").upper()
-        elif input == "S":
-            print("Siver Service Taxi")
-            menu = input(
-                "Select a taxi from the list below to see exact details.\n(B)asic Taxi\n(S)ilver Service Taxi\n(Q)uit\n>>>").upper()
+    current_taxi = None
+    total_bill = 0
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    print("Let's drive!")
+    print(MENU)
+    menu_choice = input(">>> ").upper()
+    while menu_choice != "Q":
+        if menu_choice == "C":
+            print("Taxis available: ")
+            display_taxis(taxis)
+            try:
+                taxi_choice = int(input("Choose taxi: "))
+                while taxi_choice > len(taxis) or taxi_choice < 1:
+                    print("Taxi is not listed. Please choose from available taxis.")
+                    print("Taxis available: ")
+                    display_taxis(taxis)
+                    taxi_choice = int(input("Choose taxi: "))
+                current_taxi = taxis[taxi_choice - 1]
+                print("Bill to date: ${}".format(total_bill))
+                print(MENU)
+                menu_choice = input(">>> ").upper()
+            except ValueError:
+                print("Invalid input. Please choose from available taxis.")
+        elif menu_choice == "D" and current_taxi != None:
+            current_taxi.start_fare()
+            drive_distance = float(input("Drive how far? "))
+            current_taxi.drive(drive_distance)
+            trip_cost = current_taxi.get_fare()
+            print("Your {} trip cost you {}".format(current_taxi.name, trip_cost))
+            total_bill += trip_cost
+            print("Bill to date: ${}".format(total_bill))
+            print(MENU)
+            menu_choice = input(">>> ").upper()
+        elif menu_choice == "D" and current_taxi == None:
+            print("You can't drive without first having selected a taxi.")
+            print(MENU)
+            menu_choice = input(">>> ").upper()
         else:
-            print("Invalid input")
-            menu = input(
-                "Select a taxi from the list below to see exact details.\n(B)asic Taxi\n(S)ilver Service Taxi\n(Q)uit\n>>>").upper()
+            print("Invalid input.")
+            print(MENU)
+            menu_choice = input(">>> ").upper()
+    print("Total trip cost ${}".format(total_bill))
+    print("Taxis are now:")
+    display_taxis(taxis)
+
+def display_taxis(taxis):
+    """Display numbered list of taxis."""
+    for i, taxi in enumerate(taxis):
+        print("{} - {}".format(i+1, str(taxi)))
 
 
 main()
